cb59185729b2a014f6549d2415d40340cc0343e8
DenisMullin <kut.muu@inbox.ru>
2021-10-08 02:08:41 +0300

    init commit

--- script/spoiler.js
+++
@@ -1,8 +0,0 @@
-function spoilerdemo20() {
-	display = document.getElementById('demo20').style.display;
-	if (display == 'none') {
-		document.getElementById('demo20').style.display = 'block';
-	} else {
-		document.getElementById('demo20').style.display = 'none';
-	}
-}
