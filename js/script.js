d8c414371704571fff2cebdc78662eb63fcfff40
DenisMullin <kut.muu@inbox.ru>
2021-10-24 22:07:18 +0300

    init commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ function showAnswerForm() {
 		document.getElementById("5").hidden = false;
 	}	
 }
+
+function showAnswerForm1() {
+	if (document.getElementById("6").hidden == false) {
+		document.getElementById("6").hidden = true;
+	}
+	else {
+		document.getElementById("6").hidden = false;
+	}	
+}
