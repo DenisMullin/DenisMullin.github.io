b0717e77840cf9f6d91320d4010284301a332e93
denis <d.mullin@iac.spb.ru>
2024-04-05 19:07:31 +0300

    qweqweqw

--- script.js
+++ script.js
@@ -1,69 +1,21 @@
-function printObj(obj, level = -1, lk = false) {
-    level += 1;
-    let oneSpace = '    ';
-    let space = oneSpace.repeat(level);
-    let arKeys = Object.keys(obj);
-    let emptyObj = arKeys.length === 0;
-    let lK = arKeys.slice(-1);
-    let fNL = emptyObj ? '' : '\n';
-    let nS = emptyObj ? '' : space;
-    let res = `${fNL}${nS}{${fNL}`;
-    let lkk = '';
-    for (const [key, value] of Object.entries(obj)) {
-        if (typeof value === 'object' && !Array.isArray(value)) {
-            res += `${oneSpace}${space}${key}: ${printObj(value, level, lK != key)}`;
-        } else {
-            res += `${oneSpace}${space}${key}: ${parseType(value)}${lK != key ? ',' : ''}\n`;
-        }
-        lkk = key;
+let init = () => {
+    const videoElement = document.getElementById('video');
+
+    if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function(stream) {
+            videoElement.srcObject = stream;
+            let height = videoElement.offsetHeight; // Или используйте clientHeight в зависимости от нужд
+            videoElement.style.clipPath = `circle(${height/1.5}px at center)`;
+
+            // Если вы хотите использовать проценты или другие значения, просто подставьте их в строку
+            // Например, создаем эллипс, который покрывает весь элемент
+            // element.style.clipPath = `ellipse(50% 50% at 50% 50%)`;
+        })
+        .catch(function(error) {
+        console.log("Что-то пошло не так: ", error);
+        });
     }
-    level++;
-    space = oneSpace.repeat(Math.max(level-1, 0));
-    let comma = level > 1 && lk ? ',' : '';
-    res += `${nS}}${comma}\n`;
-    return res;
-}
-function parseType(val) {
-    let res = ``;
-    switch (typeof val) {
-        case 'Number':
-            res = `${val}`
-            break;
-        case 'string':
-            res = `'${val}'`;
-            break;
-        case 'object':
-            res = `[${val}]`;
-            break;
-        default:
-            res = `${val}`;
-    }
-    return res;
 }
 
-const app = window.Telegram.WebApp;
-
-
-document.querySelector('#pre_1').innerHTML = app.initData;
-document.querySelector('#pre_2').innerHTML = (typeof app.initDataUnsafe === 'object') ? printObj(app.initDataUnsafe) : app.initDataUnsafe;
-document.querySelector('#pre_3').innerHTML = app.platform;
-document.querySelector('#pre_4').innerHTML = app.version;
-document.querySelector('#pre_5').innerHTML = app.viewportHeight;
-document.querySelector('#pre_6').innerHTML = app.viewportStableHeight;
-
-
-document.querySelector('#expand').addEventListener('click', () => {
-    app.expand();
-});
-
-document.querySelector('#showInitData').addEventListener('click', () => {
-    let block = document.querySelector('#initData');
-    if (!block.classList.contains('hidden')) {
-        block.classList.add('hidden');   
-    } else {
-        block.classList.remove('hidden');
-    }
-});
-
-app.BackButton.show();
-app.MainButton.show();
+document.addEventListener('DOMContentLoaded', init);
